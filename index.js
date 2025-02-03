40b9ba4fa2b278b6ff02753cda4fd0d04f67e9b9
kingson12 <ngalakingson@gmail.com>
2025-02-03 02:34:43 -0800

    commit this new change

--- index.js
+++ index.js
@@ -20,10 +20,8 @@ const fourthHexCode = document.querySelector('#fourth-hex-code')
 const hexaDecimal= [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
 
 menuOpen.addEventListener('click', ()=>{
-    if(menuList.style.maxHeight == '0px'){
+   
         menuList.style.maxHeight = '360px'
-
-    }
 
     if (menuOpen){
         menuOpen.style.display= 'none'
